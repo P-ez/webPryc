f63f7c7c8a87cd1f5c3b82b953a4f68c6c2a2993
P-ez <dg.mendoza.27@gmail.com>
2021-10-25 17:00:21 -0300

    initial
    first commit

---
+++ tp.js
@@ -0,0 +1,8 @@
+let boton=document.querySelector('.nav-button')
+let desplegable=document.querySelector('.oculto')
+
+boton.addEventListener('click',()=>{
+    oculto.classList.toggle(desplegable);
+}
+
+)
