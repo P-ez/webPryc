b5f5e0e0639f405d7e8d3fec5cb2fd87cd8d80a5
P-ez <dg.mendoza.27@gmail.com>
2022-01-09 01:36:39 -0300

    feat: footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { GridComponent } from './components/grid/grid.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    GridComponent
+    GridComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
