aa9885b75757a6f89581601557f84ffc82148ad0
P-ez <dg.mendoza.27@gmail.com>
2022-01-09 01:20:49 -0300

    feat: grid index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
+import { GridComponent } from './components/grid/grid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    GridComponent
   ],
   imports: [
     BrowserModule,
