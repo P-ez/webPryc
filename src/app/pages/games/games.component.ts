5a579ae9eab8d6f87fe405eff76c883508e8b09c
P-ez <dg.mendoza.27@gmail.com>
2022-01-10 23:52:45 -0300

    feat:se acabo el tiempo

--- src/app/pages/games/games.component.ts
+++ src/app/pages/games/games.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  } from '@angular/core';
 import Swiper, { Navigation, Pagination } from 'swiper';
 import 'swiper/scss';
 import 'swiper/scss/navigation';
@@ -10,8 +10,9 @@ import 'swiper/scss/pagination';
   styleUrls: ['./games.component.scss']
 })
 export class GamesComponent implements OnInit {
- 
 
+imagen = "assets/images/bckg_parn.jpg";
+imagen2 = "assets/images/game.svg";
   constructor() {
     Swiper.use([Navigation,Pagination])
    }
